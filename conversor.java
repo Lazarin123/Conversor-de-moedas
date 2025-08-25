7a4245002a9fabb41eb6d4ecabf74f809737c9f6
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-08-25 20:36:19 -0300

    criaçã de estruturas

--- conversor.java
+++ conversor.java
@@ -1,4 +1,17 @@
 public class conversor {
     public static void main(String[] args) {    
-        
+        System.out.println("Olá, seja bem-vindo(a)!\n");
+        System.out.println("Esse é um projeto simples em Java!\n");  
+        System.out.println("Aqui testo um pouco do meu conhecimento em java criando um programa de conversão de moedas!\n"); 
+        System.out.println("Espero que goste!\n");
+
+        int escolha;
+        System.out.println("Escolha (1) para converter Real para Dólar\n");
+        System.out.println("Escolha (2) para converter Dólar para Real\n");
+        System.out.println("Escolha (3) para converter Real para Euro\n");
+        System.out.println("Escolha (4) para converter Euro para Real\n");
+        System.out.println("Escolha (5) para converter Euro para Dólar\n");
+        System.out.println("Escolha (6) para converter Dólar para Euro\n");
+        System.out.println("Qual deseja converter? \n");
+        escolha = new java.util.Scanner(System.in).nextInt();
 }
