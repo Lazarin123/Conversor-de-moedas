030766c762ec9966f11ca578bd460c0112c74886
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-08-25 20:35:38 -0300

    testando

--- conversor.java
+++ conversor.java
@@ -1,3 +1,7 @@
 public class conversor {
-    
+    public static void main(String[] args) {    
+        System.out.println("Olá, seja bem-vindo(a)!\n");
+        System.out.println("Esse é um projeto simples em Java!\n");  
+        System.out.println("Aqui testo um pouco do meu conhecimento em java criando um programa de conversão de moedas!\n"); 
+        System.out.println("Espero que goste!\n");
 }
