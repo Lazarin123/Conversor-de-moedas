7f7b949df24f98efec3e480278c496fce16c6fd8
Samuel Lazarin <128560645+Lazarin123@users.noreply.github.com>
2025-10-29 10:15:56 -0300

    Ajustando

--- conversor.java
+++ conversor.java
@@ -5,8 +5,16 @@ public class conversor {
         System.out.println("Aqui testo um pouco do meu conhecimento em java criando um programa de conversão de moedas!\n"); 
         System.out.println("Espero que goste!\n");
 
-       
-
+        int escolha;
+        System.out.println("Escolha (1) para converter Real para Dólar\n");
+        System.out.println("Escolha (2) para converter Dólar para Real\n");
+        System.out.println("Escolha (3) para converter Real para Euro\n");
+        System.out.println("Escolha (4) para converter Euro para Real\n");
+        System.out.println("Escolha (5) para converter Euro para Dólar\n");
+        System.out.println("Escolha (6) para converter Dólar para Euro\n");
+        System.out.println("Qual deseja converter? \n");
+        escolha = new java.util.Scanner(System.in).nextInt();
+        
         switch (escolha) {
             case 1:
                 System.out.println("Você escolheu converter Real para Dólar\n");
