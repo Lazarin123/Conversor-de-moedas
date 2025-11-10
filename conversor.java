ebc05a0a076bd3040fffca042f19b7824ba2e10e
Samuel Lazarin <128560645+Lazarin123@users.noreply.github.com>
2025-11-10 10:21:15 -0300

    Remove welcome messages from conversor
    Removed introductory messages from main method.

--- conversor.java
+++ conversor.java
@@ -1,9 +1,4 @@
-public class conversor {
-    public static void main(String[] args) {    
-        System.out.println("Olá, seja bem-vindo(a)!\n");
-        System.out.println("Esse é um projeto simples em Java!\n");  
-        System.out.println("Aqui testo um pouco do meu conhecimento em java criando um programa de conversão de moedas!\n"); 
-        System.out.println("Espero que goste!\n");
+
 
         int escolha;
         System.out.println("Escolha (1) para converter Real para Dólar\n");
